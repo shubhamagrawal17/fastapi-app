057a1f05399fb3c793e185841b5eaca041c68c1b
Shubham Agrawal <subhek.agrawal@gmail.com>
2026-05-09 23:58:53 +0530

    Update auth.py

--- app/api/v1/auth.py
+++ app/api/v1/auth.py
@@ -1,21 +1,33 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException
+
 from app.core.security import create_access_token
+
+load_dotenv()
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
-# fake user (demo only)
-fake_user = {
-    "username": "admin",
-    "password": "admin123"
-}
+USERNAME = os.getenv("APP_USERNAME")
+PASSWORD = os.getenv("APP_PASSWORD")
 
 
 @router.post("/login")
 def login(data: dict):
-    if data.get("username") != fake_user["username"] or data.get("password") != fake_user["password"]:
-        raise HTTPException(status_code=401, detail="Invalid credentials")
 
-    token = create_access_token({"sub": data["username"]})
+    if (
+        data.get("username") != USERNAME
+        or data.get("password") != PASSWORD
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid credentials"
+        )
+
+    token = create_access_token({
+        "sub": data["username"]
+    })
 
     return {
         "access_token": token,
